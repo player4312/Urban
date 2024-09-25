7f1f6a53caab3dfd4ff78c08d41322a4a9a28d31
player4312 <hesherrrr@gmail.com>
2024-09-26 02:58:15 +0400

    26.09.2024

---
+++ Exercise_leeson_2.py
@@ -0,0 +1,17 @@
+#name = 'Konstantin'
+#print(name)
+#age = 27
+#print(age)
+#new_age = 33
+#print(new_age)
+#is_student = True
+#print(is_student)
+
+name = 'Konstantin'
+print('Name:', name)
+age = 27
+print('Age:', age)
+new_age = 33
+print('New Age:', new_age)
+is_student = True
+print('Is student:', is_student)
---
+++ leeson_2_DynamicTyping.py
@@ -0,0 +1,8 @@
+name = "leeson" # динамическая типазация
+print(name, type(name))
+name = 5
+print(name ,type(name))
+name = 5.5
+print(name, type(name))
+name = [1, 2]
+print(name, type(name))
