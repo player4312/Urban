d73a3f5b3eb558240fe7c450a35bc1e125dc060a
player4312 <hesherrrr@gmail.com>
2024-09-26 20:50:26 +0400

    26.09.2024

--- Exercise_leeson_3.py
+++ Exercise_leeson_3.py
@@ -2,7 +2,7 @@ number_of_homework_completed = 12
 number_of_hours_spent = 1.5
 course_name = 'Python'
 time_for_one = number_of_hours_spent / number_of_homework_completed
-print('Курс:',course_name ,
+print('Курс:',course_name,
       ', всего задач:',number_of_homework_completed,
       ', затрачено часов:',number_of_hours_spent,
       ', среднее время выполнения:',time_for_one,
