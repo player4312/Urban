ed8941347a4d3bb0a8f8d5e74e57b171abd6c0c3
player4312 <hesherrrr@gmail.com>
2024-10-03 23:23:05 +0400

    03.10.2024

--- Exercise_module_2_program_line.py
+++ Exercise_module_2_program_line.py
@@ -1,14 +1,14 @@
 #1st program
 my_string = 'Сколько символов в строке?:'
-end = 'Если ввел 24, то это верно!' # пробелы так же идут в счет
+end = 'Если ввел 24, то это верно!'
 number = len(my_string) - my_string.count(' ')
 print('Всего символов в строке', number)
 print(input(my_string))
 print(end)
 
 #2st program
-# print('Сколько символов в строке?:' .upper())
-# print('Сколько символов в строке?:' .lower())
-# print('Сколько символов в строке?:' .replace(' ',''))
-# print(my_string[0])
-# print(my_string[26])
+print('Сколько символов в строке?:' .upper())
+print('Сколько символов в строке?:' .lower())
+print('Сколько символов в строке?:' .replace(' ',''))
+print(my_string[0])
+print(my_string[26])
