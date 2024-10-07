3a29a7028b6b21172631dca66bcf1c4c8ac4975c
player4312 <hesherrrr@gmail.com>
2024-10-07 20:57:19 +0400

    07.10.2024

---
+++ Module_2_Indexing_an_ListMethods.py
@@ -0,0 +1,22 @@
+#Замена первого элемента
+food = ['apple', 'coconut', 'banana', 'peach']
+print(food[0])
+food[0] = 'kek'
+print(food)
+
+#Добавление элемента с другим типом данных
+food = ['apple', 'coconut', 'banana', 'peach']
+food.append(True) #позволяет добавить элемент в конец списка
+food.append(1)
+food.append('prosto tak')
+food.extend('kuda') #перебор элементов в конце списка
+food.extend(['kuda', 2]) #добавленная строка сама стала элементом последовательности
+print(food)
+
+#Удалить из списка
+food.remove('banana') #удаление из списка
+print(food)
+
+#Проверка, есть ли в списке указанный элемент
+print('kuda' in food)
+print('kuda' not in food)
